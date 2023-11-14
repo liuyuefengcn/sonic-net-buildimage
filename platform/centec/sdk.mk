7905600124bc19ca9d7129f473663961a85b8a32
leon <13018035@qq.com>
2023-11-14 16:48:00 +0800

    冲突手工合入SHA-1: 5f9e37b36d75d2521bc857752a499d1fdc3ba643
    * update sai to v1.7.1-4 for all centec platform(X86 CPU) and centec-arm64 platform(ARM64 CPU)

--- platform/centec/sdk.mk
+++ platform/centec/sdk.mk
@@ -1,5 +1,5 @@
 # Centec SAI
-CENTEC_SAI = libsai_1.6.3-1_amd64.deb
+CENTEC_SAI = libsai_1.7.1-4_amd64.deb
 $(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/amd64/$(CENTEC_SAI)
 $(eval $(call add_conflict_package,$(CENTEC_SAI),$(LIBSAIVS_DEV)))
 
